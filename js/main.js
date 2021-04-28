94f2947871024bdaa51e5bc7dfa70f8c7a5515cd
Matheus Teixeira <matheus.teixeira@itenabler.com.br>
2021-04-28 15:28:49 -0300

    Funcionalidade de adicionar novos pacientes

--- js/main.js
+++ js/main.js
@@ -32,3 +32,41 @@ for (let paciente of pacientes){
         imc.textContent = (peso.textContent/Math.pow(altura.textContent, 2)).toFixed(2);
     }
 }
+
+/* Adicionando novo paciente */
+
+const formSubmit = document.getElementById("adicionar-paciente");
+formSubmit.addEventListener("click", (e)=>{
+    e.preventDefault();
+
+    const form = document.getElementById("form-add");
+    const table = document.getElementById("tabela-pacientes");
+
+    const altura = form.altura.value;
+    const peso = form.peso.value;
+    const nome = form.nome.value;
+    const gordura = form.gordura.value;
+
+    const pacienteTr = document.createElement("tr");
+
+    const alturaTd = document.createElement("td");
+    const pesoTd = document.createElement("td");
+    const nomeTd = document.createElement("td");
+    const gorduraTd = document.createElement("td");
+    const imcTd = document.createElement("td");
+
+    alturaTd.textContent = altura;
+    pesoTd.textContent = peso;
+    nomeTd.textContent = nome;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    table.appendChild(pacienteTr);
+
+
+});
