de2806de97e4d6ba809fb04b928b7e9e6bba471c
Matheus Teixeira <matheus.teixeira@itenabler.com.br>
2021-04-28 11:41:06 -0300

    Realizando o cálculo do imc para cada paciente

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+//Alteração de título
+
 const obj = document.getElementById("title");
-obj.textContent = "Enabler Nutrição"
+obj.textContent = "Enabler Nutrição";
+
+const pacientes = document.querySelectorAll("#tabela-pacientes tr");
+
+//Cálculo de IMC
+
+for (paciente of pacientes){
+    const peso = parseInt(paciente.querySelector(".info-peso").textContent);
+    const altura = parseInt(paciente.querySelector(".info-altura").textContent);
+
+    let imc = paciente.querySelector(".info-imc");
+    
+    imc.textContent = peso/altura*altura
+}
