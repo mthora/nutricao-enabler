ce585a5cd6f1f2fd97ebe1b2e1afa19a4a743c69
Matheus Teixeira <matheus.teixeira@itenabler.com.br>
2021-04-28 13:16:33 -0300

    Validação de peso e altura antes do cálculo de IMC

--- js/main.js
+++ js/main.js
@@ -8,10 +8,29 @@ const pacientes = document.querySelectorAll("#tabela-pacientes tr");
 //Cálculo de IMC
 
 for (paciente of pacientes){
-    const peso = paciente.querySelector(".info-peso").textContent;
-    const altura = paciente.querySelector(".info-altura").textContent;
 
+    const peso = paciente.querySelector(".info-peso");
+    const altura = paciente.querySelector(".info-altura");
     let imc = paciente.querySelector(".info-imc");
+
+    let pesoValido = true;
+    let alturaValida = true;
     
-    imc.textContent = Math.floor(peso/(altura*altura));
+    if (peso.textContent < 0 || peso.textContent >=600){
+        imc.textContent = "Peso inválido!";
+        peso.style.color = "red";
+        imc.style.color = "red";
+        pesoValido = false;
+    } 
+    
+    if (altura.textContent < 0 || altura.textContent >= 3){
+        imc.textContent = "Altura inválida!"
+        altura.style.color = "red";
+        imc.style.color = "red";
+        alturaValida = false;
+    }
+
+    if (pesoValido && alturaValida){
+        imc.textContent = Math.floor(peso.textContent/Math.pow(altura.textContent, 2));
+    }
 }
